1192ec161f8ab08d37a70ef4447f0006ca1af605
Peter Rindal <peterrindal@gmail.com>
2023-11-23 16:54:15 -0800

    mac compile fix 2

--- libOTe/Tools/Subfield/SubfieldPprf.h
+++ libOTe/Tools/Subfield/SubfieldPprf.h
@@ -1221,7 +1221,7 @@ namespace osuCrypto::Subfield
                             //    H(x) = (AES(k0, x) + x) || (AES(k1, x) + x);
                             //
                             // where each half defines one of the children.
-                            aes[keep].hashBlocks<8>(parent.data(), child.data());
+                            aes[keep].template hashBlocks<8>(parent.data(), child.data());
 
 
 
